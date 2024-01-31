f326b4b61617ec64b2103c2cf32e584bf3c3d1f1
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-01-31 12:34:27 +0100

    fix pbits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -98,6 +98,6 @@ var store = [{
         "title": "The Time I Built a Probabilistic Computer",
         "excerpt":"Digital computing, with its binary system of bits, has been the bedrock of technological advancement for decades. This deterministic approach, where bits are either 0 or 1, has propelled us into an era of unprecedented computational capabilities. However, as we stand on the precipice of a new computing frontier, the...","categories": ["unconventionalcomputing"],
         "tags": [],
-        "url": "/unconventionalcomputing/2024/01/19/pbits.html",
+        "url": "/unconventionalcomputing/2024/01/31/pbits.html",
         "teaser": null
       }]
